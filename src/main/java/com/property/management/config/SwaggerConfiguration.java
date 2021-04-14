f82d05eef1d04eac4983d11bca2a291e1eff3047
Uday <udp@narola.email>
2021-04-14 16:10:59 +0530

    add header in swagger

--- src/main/java/com/property/management/config/SwaggerConfiguration.java
+++ src/main/java/com/property/management/config/SwaggerConfiguration.java
@@ -2,15 +2,19 @@ package com.property.management.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -19,6 +23,8 @@ public class SwaggerConfiguration {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .forCodeGeneration(true)
+                .globalOperationParameters(globalParameterList())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
@@ -36,6 +42,19 @@ public class SwaggerConfiguration {
                 "",
                 "",
                 Collections.emptyList());
+    }
+
+    private List<Parameter> globalParameterList() {
+        Parameter authTokenHeader =
+                new ParameterBuilder()
+                        .name("api-key")
+                        .modelRef(new ModelRef("string"))
+                        .required(true)
+                        .parameterType("header")
+                        .description("Api key for authentication")
+                        .build();
+
+        return Collections.singletonList(authTokenHeader);
     }
 
 }
